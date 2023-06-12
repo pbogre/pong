18a374d06dcb843d37179be9239baa7bc3f74e6a
Pietro Bonaldo <pietro.bogre@gmail.com>
2023-06-12 13:56:39 +0200

    improve desync warning system

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -64,14 +64,39 @@ int main(){
 		cout << "Starting game #" << game_count << endl;
 
 		thread t1([&]() {
+			int score_desync_counter = 0;
+			int ball_desync_counter = 0;
 			while (clients[0].status == sf::Socket::Done) {
 				sf::Packet p;
 				clients[0].status = clients[0].socket.receive(p);
 				p >> clients[0].pos >> clients[0].bx >> clients[0].by >> clients[0].bdx >> clients[0].bdy >> clients[0].scores[0] >> clients[0].scores[1];
 
+				// only need one thread to warn about desync
+				// use counter to only warn about sustained desync
 				if(clients[0].scores[0] != clients[1].scores[0] || clients[0].scores[1] != clients[1].scores[1]){
-				  cout << "Score desync!" << endl;
+				  score_desync_counter++;
+				  if(score_desync_counter >= 10){
+				    cout << "Score desync!" << endl;
+				    score_desync_counter = 0;
+				  }
 				}
+				else score_desync_counter = 0;
+
+				// this abomination basically returns true if the 
+				// ball's position or velocity is different by a relatively
+				// significant margin
+				if( (clients[0].bx + 5 < clients[1].bx || clients[0].bx - 5 > clients[1].bx ) || 
+				    (clients[0].by + 5 < clients[1].by || clients[0].by - 5 > clients[1].by)  ||
+				    (clients[0].bdx + 1 < clients[1].bdx || clients[0].bdx - 1 > clients[1].bdx) ||
+				    (clients[0].bdy + 1 < clients[1].bdy || clients[0].bdy - 1 > clients[1].bdy) 
+				  ){
+				  ball_desync_counter++;
+				  if(ball_desync_counter >= 50){
+				    cout << "Ball desync!" << endl;
+				    ball_desync_counter = 0;
+				  }
+				}
+				else ball_desync_counter = 0;
 
 				clients[1].socket.send(p);
 				p.clear();
@@ -87,13 +112,6 @@ int main(){
 				sf::Packet p;
 				clients[1].status = clients[1].socket.receive(p);
 				p >> clients[1].pos >> clients[1].bx >> clients[1].by >> clients[1].bdx >> clients[1].bdy >> clients[1].scores[1] >> clients[1].scores[0];
-
-				if(clients[0].scores[0] != clients[1].scores[0] || clients[0].scores[1] != clients[1].scores[1]){
-				  cout << "Score desync!" << endl;
-				}
-				if(clients[0].bx != clients[1].by || clients[0].by != clients[1].by || clients[0].bdx != clients[1].bdx || clients[0].bdy != clients[1].bdy){
-				  cout << "Ball desync!" << endl;
-				}
 
 				clients[0].socket.send(p);
 				p.clear();
